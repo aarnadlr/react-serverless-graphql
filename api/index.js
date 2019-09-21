4f0200a307d497cf4e58ed18510e0dc3c31aca52
aarnadlr <aaronadler@gmail.com>
2019-09-20 20:13:27 -0400

    Add additional queries

--- api/index.js
+++ api/index.js
@@ -2,17 +2,29 @@ const { ApolloServer, gql } = require('apollo-server-micro')
 
 const typeDefs = gql`
   type Query {
-    hello: String
+    hello: String,
+    name: String,
+    age: Int,
+    color: String
   }
 `
 
 const resolvers = {
   Query: {
     hello: (root, args, context) => {
-      return 'Hello world!'
+      return 'Hello world!';
+    },
+    name: (root, args, context) => {
+      return 'Asher!';
+    },
+    age: (root, args, context) => {
+      return 8;
+    },
+    color: (root, args, context) => {
+      return 'navy blue!';
     }
   }
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
