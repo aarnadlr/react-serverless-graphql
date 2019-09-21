368994b5f55a1b53525fd0ad554acef70ae7e362
aarnadlr <aaronadler@gmail.com>
2019-09-21 16:31:59 -0400

    Add new sample data. Revise .nowignore

--- api/index.js
+++ api/index.js
@@ -1,30 +1,31 @@
 require('dotenv').config();
-const { ApolloServer, gql } = require('apollo-server-micro')
+const { ApolloServer, gql } = require('apollo-server-micro');
 
-// The API: Queries, Muts, Subs which are available:
+const books = [
+  {
+    title: 'Book title 1',
+    author: 'Asher'
+  },
+  {
+    title: 'Book title2',
+    author: 'Charley'
+  }
+];
+
+// The API: All Queries, Mutations, Subs available:
 const typeDefs = gql`
   type Query {
-    hello: String,
-    name: String,
-    age: Int,
-    color: String
+    books:[Book]
   }
-`
+  type Book{
+    title:String
+    author:String
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: (root, args, context) => {
-      return 'Hello world!';
-    },
-    name: (root, args, context) => {
-      return 'Asher!';
-    },
-    age: (root, args, context) => {
-      return 8;
-    },
-    color: (root, args, context) => {
-      return 'navy blue!';
-    }
+    books:()=>books
   }
 };
 
@@ -33,9 +34,9 @@ const server = new ApolloServer({
   resolvers,
   introspection: true,
   playground: true,
-	engine: {
+  engine: {
     apiKey: process.env.ENGINE_API_KEY
-  },
-})
+  }
+});
 
-module.exports = server.createHandler({ path: '/api' })
+module.exports = server.createHandler({ path: '/api' });
