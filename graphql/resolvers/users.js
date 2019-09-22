b22fadd49994a7719d5f3c89bbd8682295965760
aarnadlr <aaronadler@gmail.com>
2019-09-22 09:23:21 -0400

    Add validation: make sure user doesnt already exist

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-
+const { UserInputError } = require('apollo-server');
+const Post = require('../../models/Post');
 const User = require('../../models/User');
 
 module.exports = {
@@ -14,31 +15,43 @@ module.exports = {
       context,
       info
     ) {
-      //Validate user data
+      // Validate user data
       // Make sure user doesnt already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError('Username is taken', {
+          errors: {
+            username: 'This username is taken'
+          }
+        });
+      }
       // Hash password and create auth token
-			password = await bcrypt.hash(password, 12);
+      password = await bcrypt.hash(password, 12);
 
-			const newUser = new User({
-				email,
-				username,
-				password,
-				createdAt: new Date().toISOString()
-			});
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      });
 
-			const res = await newUser.save();
+      const res = await newUser.save();
 
-			const token = jwt.sign({
-				id: res.id,
-				email: res.email,
-				username: res.username
-			}, process.env.SECRET_KEY, { expiresIn: '1h'});
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username
+        },
+        process.env.SECRET_KEY,
+        { expiresIn: '1h' }
+      );
 
-			return {
-				...res._doc,
-				id: res._id,
-				token
-			}
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      };
     }
   },
 
